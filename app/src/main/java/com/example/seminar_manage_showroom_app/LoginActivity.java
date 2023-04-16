40e111b38c2c4b63cb0b6646ff59a0ac66f6e4dd
Vũ Luân Nguyễn <113601311+LuanVuNguyen@users.noreply.github.com>
2023-04-16 20:17:33 +0700

    update_button

--- app/src/main/java/com/example/seminar_manage_showroom_app/LoginActivity.java
+++ app/src/main/java/com/example/seminar_manage_showroom_app/LoginActivity.java
@@ -2,13 +2,50 @@ package com.example.seminar_manage_showroom_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView btn_Login;
+    private Button btn_Signup, btn_Guest;
+    private Context mActivity = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mActivity = this;
         setContentView(R.layout.activity_login);
+        Init();
     }
+
+    private void Init()
+    {
+        btn_Signup = (Button) findViewById(R.id.btn_creat);
+        btn_Signup.setOnClickListener(this);
+        btn_Login = (ImageView) findViewById(R.id.btn_login);
+        btn_Login.setOnClickListener(this);
+        btn_Guest = (Button) findViewById(R.id.btn_login_guest);
+        btn_Guest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.btn_creat:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+            case R.id.btn_login:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+            case R.id.btn_login_guest:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/seminar_manage_showroom_app/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.seminar_manage_showroom_app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+    }
+}
+
