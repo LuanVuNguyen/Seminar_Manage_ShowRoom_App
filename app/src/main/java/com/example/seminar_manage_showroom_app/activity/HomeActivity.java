0c0a577d8b3c92e13982bbd9e1ef0084d7f8f38f
LuanNguyenVu <luannguyenvu06@gmail.com>
2023-04-18 15:41:38 +0700

    update screens (home, inven, CreateProductActivity)

--- app/src/main/java/com/example/seminar_manage_showroom_app/activity/HomeActivity.java
+++ app/src/main/java/com/example/seminar_manage_showroom_app/activity/HomeActivity.java
@@ -2,16 +2,18 @@ package com.example.seminar_manage_showroom_app.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.seminar_manage_showroom_app.R;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView btn_search,btn_pay,btn_inventory,btn_creproduct,btn_import,btn_export,btn_setting,btn_profile,btnlogout;
+    private ImageView btn_search,btn_pay,btn_inventory,btn_creproduct,btn_import,btn_export,btn_setting,btn_profile,btn_logout,avatar;
 
 
     @Override
@@ -19,6 +21,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         init();
+        Glide.with(this)
+                .load("https://i.pinimg.com/736x/68/ea/e5/68eae5110003466af047764ff88e2403.jpg")
+                .into(avatar);
     }
 
     private void init()
@@ -47,8 +52,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         btn_profile = (ImageView) findViewById(R.id.btn_profile);
         btn_profile.setOnClickListener(this);
 
-        btnlogout = (ImageView) findViewById(R.id.btn_logout);
-        btnlogout.setOnClickListener(this);
+        btn_logout = (ImageView) findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(this);
+
+        avatar = (ImageView) findViewById(R.id.img_avatar);
+        avatar.setOnClickListener(this);
     }
 
     @Override
